296b048601e2968070f850216cf69ec991c05742
서효정 <105341847+hyojjamppong@users.noreply.github.com>
2022-11-06 22:46:58 +0900

    Update 불량 사용자.py

--- 2019 카카오 개발자 겨울 인턴십/불량 사용자.py
+++ 2019 카카오 개발자 겨울 인턴십/불량 사용자.py
@@ -1,1 +1,24 @@
 from itertools import permutations
+
+def check(user, ban):
+    for u, b in zip(user, ban):
+        if len(u) != len(b):
+            return False
+        
+        for i in range(len(u)):
+            if u[i] != b[i] and b[i] != '*':
+                return False
+            
+     return True
+
+def solution(user_id, banned_id):
+    answer = []
+    candidates = list(permutations(user_id, len(banned_id))
+    
+    for candidate in candidates:
+        if check(candidate, banned_id):
+            candidate = sorted(candidate)
+            if candidate not in answer:
+                answer.append(candidate)
+    
+    return len(answer)
